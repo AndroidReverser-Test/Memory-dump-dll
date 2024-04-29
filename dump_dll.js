f76f1186bc9885ad00963bbc2d7731c9314c0c8c
feilongzaitia <A2322146826@163.com>
2024-04-29 13:37:36 +0800

    完善一些细节

--- dump_dll.js
+++ dump_dll.js
@@ -3,19 +3,19 @@ rpc.exports = {
         let results = [];
         let sizes = [];
         let size=0;
-        let s0,s1,s2;
+        let snumber=0;
         let pattern = "50 45 00 00";
         let max_size = 0x61A8000;//默认dll的最大值为100m
         send("searching");
         Process.enumerateRanges('r--').forEach(function (range) {
             try {
                 Memory.scanSync(range.base, range.size,pattern).forEach(function (match) {
-                    if(get_hex_str(match.address.add(0x18).readByteArray(2)).indexOf("0b01")!=-1){
+                    if(match.address.add(0x18).readShort()==0x10b){
+                        snumber = match.address.add(0x6).readShort();
                         size = 0x180;
-                        s0 = match.address.add(0xf8).add(16).readInt();
-                        s1 = match.address.add(0x120).add(16).readInt();
-                        s2 = match.address.add(0x148).add(16).readInt();
-                        size = size+s0+s1+s2;
+                        for(let k=0;k<snumber;k++){
+                            size += match.address.add(0xf8).add(16).add(k*0x28).readInt();
+                        }
                         if(size>0 && size<max_size){
                             results.push(match.address);
                             sizes.push(size);
@@ -41,16 +41,3 @@ rpc.exports = {
 }
 
 
-function get_hex_str(arrayBuffer){
-        let il_str = "";
-        let ilArray = Array.prototype.slice.call(new Uint8Array(arrayBuffer));
-        let byt;
-        for(let i=0;i<ilArray.length;i++){
-            byt = ilArray[i]
-            if(byt<16){
-                il_str += "0";
-            }
-            il_str += byt.toString(16);
-        }
-        return il_str;
-    }
